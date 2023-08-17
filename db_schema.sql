7c3c854fd44ff571cfa9599bfe74880d197a9bce
vannesDhar <vannesdharmawan3@gmail.com>
2023-08-17 15:46:44 +0800

    git

--- db_schema.sql
+++ db_schema.sql
@@ -4,24 +4,13 @@ PRAGMA foreign_keys=ON;
 
 BEGIN TRANSACTION;
 
---create your tables with SQL commands here (watch out for slight syntactical differences with SQLite)
-
-CREATE TABLE IF NOT EXISTS testUsers (
-    test_user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    test_name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS Stories(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    timestamp DATETIME NOT NULL,
+    country TEXT NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS testUserRecords (
-    test_record_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    test_record_value TEXT NOT NULL,
-    test_user_id  INT, --the user that the record belongs to
-    FOREIGN KEY (test_user_id) REFERENCES testUsers(test_user_id)
-);
-
---insert default data (if necessary here)
-
-INSERT INTO testUsers ("test_name") VALUES ("Simon Star");
-INSERT INTO testUserRecords ("test_record_value", "test_user_id") VALUES( "Lorem ipsum dolor sit amet", 1); --try changing the test_user_id to a different number and you will get an error
 
 COMMIT;
 
