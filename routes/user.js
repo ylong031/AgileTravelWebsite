0c86a551e95e04664557b2306f280767dd67a21b
vannesDhar <vannesdharmawan3@gmail.com>
2023-08-23 14:53:10 +0800

    Added China

--- routes/user.js
+++ routes/user.js
@@ -26,6 +26,9 @@ router.get('/country/mexico', async (req, res, next) => {
 router.get('/country/italy', async (req, res, next) => {
   res.render('Italy');
 });
+router.get('/country/china', async (req, res, next) => {
+  res.render('China');
+});
 
 router.get('/games',async (req, res, next) => {
   res.render('games-page');
